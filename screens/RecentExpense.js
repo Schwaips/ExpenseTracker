6e991cd679cac6bd91c8078ac8ac1c7478d4ebcc
Charlie Bertrand <charlie.bertrand@live.com>
2022-11-03 12:16:02 +0100

    adding bottom bar

--- screens/RecentExpense.js
+++
@@ -1,7 +0,0 @@
-import { Text } from "react-native";
-
-function RecentExpense(params) {
-  return <Text>Hello Manage Expense</Text>;
-}
-
-export default RecentExpense;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function RecentExpenses(params) {
+  return <Text>Hello Recent Expense</Text>;
+}
+
+export default RecentExpenses;

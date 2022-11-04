8df838b4a24a4c215b6b7634a7b373b5f0095491
Charlie Bertrand <charlie.bertrand@live.com>
2022-11-04 11:37:06 +0100

    input config

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, View, Text} from "react-native";
 
 import Input from "./Input";
 
@@ -9,29 +9,59 @@ function ExpenseForm() {
 
 
   return (
-    <View>
-      <Input
-        label="Amount"
-        textInputConfig={{
-          keyboardType: "decimal-pad",
-          onChangeText: amountChangeHandler,
+    <View style={styles.form}>
+      <Text style={styles.title}>Your expense</Text>
+      <View style={styles.inputsRow}>
+        <Input
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: amountChangeHandler,
+          }}
+          style={styles.rowInput}
+        />
+        <Input
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChange: () => {},
+          }}
+          style={styles.rowInput}
+        />
+      </View>
 
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          // autoCorrect: false, // default is true
+          // autoCapitalize: 'sentences', // default value is sentences.
         }}
       />
-      <Input label="Date" textInputConfig={{
-        placeholder: 'YYYY-MM-DD',
-        maxLength: 10,
-        onChange: () => {},
-      }} />
-      <Input label="Description" textInputConfig={{
-        multiline: true,
-        // autoCorrect: false, // default is true
-        // autoCapitalize: 'sentences', // default value is sentences.
-      }} />
     </View>
   );
 }
 
 export default ExpenseForm;
 
-
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+    marginVertical: 24
+  },
+  inputsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  rowInput: {
+    flex: 1,
+  }
+})
